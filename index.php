8574b2480a4d467399b87a6411dc827f5a2f04da
alanna04 <t_alanna@msn.com>
2019-06-25 16:29:03 -0400

    our team section

--- index.php
+++ index.php
@@ -180,24 +180,104 @@
     </ul>
 </div>
 <!--grey section -->
-<div class="uk-section uk-section-muted">
+<div class="uk-section uk-section-default">
     <div class="uk-container">
 
-        <h3>Section</h3>
+        <h5>Our Team</h5>
 
-        <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-            <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
-            </div>
-        </div>
+       <!-- <div class="uk-grid-match uk-child-width-1-3@m" uk-grid> -->
+            <div class="uk-grid-match uk-child-width-1-1@m" uk-grid>
+              <div>
+                   <div id="our-team"></i>Liquorice croissant sugar plum macaroon jelly beans cheesecake donut tart cookie. Jelly beans icing marzipan apple pie bonbon bonbon macaroon pie pie. 
+                  </div>
+              </div>
+
+              <!-- slider card start -->
+                <div uk-slider="center: true">
+
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+
+        <ul class="uk-slider-items uk-child-width-1-2@s uk-grid">
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="/wp-content/uploads/doc3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <div class="uk-card-title staff-title">Nurse Practitioner</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="/wp-content/uploads/doc3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <div class="uk-card-title staff-title">Chiropractor</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="/wp-content/uploads/doc3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <div class="uk-card-title staff-title">Nutritionist</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="/wp-content/uploads/doc3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <div class="uk-card-title staff-title">Yoga Therapist</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="/wp-content/uploads/doc3.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <div class="uk-card-title staff-title">Manager/ Trainer</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
     </div>
+
+    <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
 </div>
+
+          </div>
+
+           <!-- <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+            </div>
+        </div> -->
+
+    </div>
+</div>  
 <!--grey section -->
 
