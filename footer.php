d3884ddf00e0ea18fb89c43b570702f731f48c9a
alanna04 <t_alanna@msn.com>
2019-06-25 15:37:09 -0400

    footer update

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 </div><!--end div id wrap-->
 
 
-<footer  class="uk-section-secondary">
+<footer  class="uk-section-primary">
 <div class="uk-container">
 
 <div class="clear"></div>
@@ -11,15 +11,26 @@
     <div><div class="footer-info" itemtype="http://schema.org/LocalBusiness" itemscope="itemscope">
 <span itemprop="name"><strong>Harmony Wellness Center</strong></span><br />
 <div itemtype="http://schema.org/PostalAddress" itemscope="itemscope" itemprop="address">
-<span itemprop="streetAddress">1201 W. Harrison Street</span><br />
+<div class="white-white-baby"><span itemprop="streetAddress">1201 W. Harrison Street</span><br />
 <span itemprop="addressLocality">Chicago</span>, <span itemprop="addressRegion">FL</span> <span itemprop="postalCode">60607</span><br />
 Phone: <span itemprop="telephone"><?php echo do_shortcode ('[primary-phone]' ); ?></span><br />
 Fax: <span itemprop="fax"><?php echo do_shortcode ('[fax]' ); ?></span><br />
-Email: <span itemprop="email"><a id="footer-email" href="mailto:<?php echo do_shortcode ('[email]' ); ?>"><?php echo do_shortcode ('[email]' ); ?></a></span>
+	Email: <span itemprop="email"><a id="footer-email" href="mailto:<?php echo do_shortcode ('[email]' ); ?>"><?php echo do_shortcode ('[email]' ); ?></a></span></div>
 </div></div>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11884.288658710362!2d-87.6496336!3d41.8697938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6033e9aa2dbdcdc!2sUniversity+of+Illinois+at+Chicago!5e0!3m2!1sen!2sus!4v1558055900936!5m2!1sen!2sus" width="300" height="250" frameborder="0" style="border:0" allowfullscreen></iframe></div>
-    <div><strong>Hours </strong><br />ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-    <div><strong>Sign Up For Our News Letter! </strong><br />ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br /><a href="https://www.myseoguys.com/" title="Local SEO" id="seo" target="_blank" rel="nofollow"><img src="/wp-content/uploads/jcidm.png" alt="Local SEO" width="225" height="74"/></a></div>
+		<div class="googlemap">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11884.288658710362!2d-87.6496336!3d41.8697938!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa6033e9aa2dbdcdc!2sUniversity+of+Illinois+at+Chicago!5e0!3m2!1sen!2sus!4v1558055900936!5m2!1sen!2sus" width="350" height="200" frameborder="0" style="border:0" allowfullscreen></iframe></div></div>
+    <div><strong>Hours </strong><br />
+		<ul class="hours">
+			<li>Monday - 9a.m. to 5p.m.</li>
+			<li>Tuesday - 9a.m. to 5p.m.</li>
+			<li>Wednesday - 9a.m. to 5p.m.</li>
+			<li>Thursday - 9a.m. to 5p.m.</li>
+			<li>Friday - Closed</li>
+			<li>Saturday - Closed</li>
+			<li>Sunday - Closed</li>
+		</ul>
+		</div>
+    <div><strong>Sign Up For Our News Letter! </strong><br /><p><div class="white-white-baby">Sweet lollipop candy canes carrot cake wafer cake pastry candy canes chocolate.</p><p>Soufflé sweet roll pastry sugar plum cotton candy lemon drops croissant. Danish apple pie icing. Pudding candy powder. Carrot cake dragée fruitcake toffee.</span></p><br /><a href="https://www.myseoguys.com/" title="Local SEO" id="seo" target="_blank" rel="nofollow"><img src="/wp-content/uploads/jcidm.png" alt="Local SEO" width="225" height="74"/></a></div>
 </div>
 </div><!--end footer-container-->
 </footer><!--end footer-->
